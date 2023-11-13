f787d606d1ad4603e1a72d08b46481cd82c058d4
Mariano <keybito11@gmail.com>
2023-11-13 00:42:10 -0800

    Initial Commit

---
+++ MarvelApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelApp
+//
+//  Created by user242581 on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by user242581 on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
