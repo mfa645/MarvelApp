8e6c89b509ac604c0813beb5cf88e6314eea5f2e
Mariano <keybito11@gmail.com>
2023-11-29 11:23:02 -0800

    Characters image fixed when large images and titles, search view bug fixed

---
+++ MarvelApp/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  MarvelApp
+//
+//  Created by user242581 on 29/11/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
